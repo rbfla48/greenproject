8e8b6d328a9489f065ab6d14c1034927107f2c38
rbfla48 <59451205+rbfla48@users.noreply.github.com>
2020-07-15 11:44:35 +0900

    태그설명 추가
    태그역할 주석추가함

--- spring/greencar/src/main/java/com/greencar/spring/HomeController.java
+++ spring/greencar/src/main/java/com/greencar/spring/HomeController.java
@@ -11,18 +11,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+//@Controller 어노테이션 => 현재클래스가 컨트롤러임을 선언
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	//@RequestMapping 어노테이션 => jsp에서 들어온 요청에 해당하는 비지니스로직 검색
+	//RequestMethod.GET => jsp에서 들어온 요청이 POST인지, GET인지 선언
+	@RequestMapping(value = "/", method = RequestMethod.GET) 
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
@@ -31,8 +28,13 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
+		//비즈니스로직에서 수행한 결과를 화면으로 보내주는 역할
+		//"serverTime"이라는 이름으로 formattedDate 전송
 		model.addAttribute("serverTime", formattedDate );
 		
+		//수행결과 응답을 어디로 보낼지 명시(jsp파일명을 의미)
+		//servlet-context에서 자동으로 home 앞에 "/WEB-INF/views"를 붙여주고(prefix)
+		//							  뒤에는 ".jsp"를 붙여준다.(suffix)
 		return "home";
 	}
 	
