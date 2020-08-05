9cc3d8193294a44f13237c823297f1b27dd78bd5
wjdaudgy123 <67942778+wjdaudgy123@users.noreply.github.com>
2020-08-05 17:09:35 +0900

    필요없는 (all)(greencar) 제거
    필요없는 부분 제거

--- spring/GreenCar/src/main/java/com/greencar/dao/login/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package com.greencar.dao.login;
-
-import com.greencar.vo.login.MemberVO;
-
-public interface MemberMapper {
-	
-	public MemberVO read(String user_email);
-	
-}
--- spring/GreenCar/src/main/java/com/greencar/vo/login/AuthVO.java
+++
@@ -1,9 +0,0 @@
-package com.greencar.vo.login;
-
-import lombok.Data;
-
-@Data
-public class AuthVO {
-	private String user_email;
-	private String authority;
-}
